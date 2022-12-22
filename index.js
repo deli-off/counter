bb0da2cb5bc8ccc9dba0757abae047455cc91220
deli-off <dzamsedovnazif1@gmail.com>
2022-12-22 20:06:16 +0500

    finished

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-let counter = document.querySelector('#counter')
-let btns = document.querySelectorAll('.btn')
+let btns = document.querySelectorAll('button[data-command]')
+let countView = document.querySelector('#counter')
 
-btns.forEach((btn) => {
+btns.forEach(btn => {
     btn.onclick = () => {
-        for (let i = 0; i <= 10; i++) {
-            counter++
-        }
+        let command = btn.getAttribute('data-command')
 
+        counter(command)
     }
 })
+
+function counter(cmd) {
+    switch (cmd) {
+        case "plus":
+            if (countView.innerHTML < 10) {
+                countView.innerHTML++
+            }
+            break;
+        case "minus":
+            if (countView.innerHTML > -10) {
+                countView.innerHTML--
+            }
+            break;
+        case "reset":
+            countView.innerHTML = 0
+            break;
+        case "random":
+            let rnd = Math.floor(Math.random() * 10)
+            countView.innerHTML = rnd
+            break;
+    }
+}
